e361ce11c0bed5e59b7fbdf4491a04c4affc972e
Mariya Malandina <mariyamalandina@gmail.com>
2022-10-28 04:56:13 +0300

    .

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.0.1', '-v, --version', 'output the version number.')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-h, --help', 'display help for command');
+
+program.parse();
